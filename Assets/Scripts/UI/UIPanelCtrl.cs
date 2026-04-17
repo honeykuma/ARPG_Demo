9f395014752ca4fbbbadf95a926d6f891532a338
honeykuma <howrd1648@gmail.com>
2026-04-17 15:38:19 +0800

    05:CanvasGroup
    使用程式控制CanvasGroup的開關，達成可操作的"退出提示介面"出現以及背景鎖定

---
+++ Assets/Scripts/UI/UIPanelCtrl.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class UIPanelCtrl : MonoBehaviour
+{
+    #region 基礎元件
+    
+    /// <summary>
+    /// CanvasGroup元件本體(盡量不直接控制)
+    /// </summary>
+    private CanvasGroup _canvasGroup;
+    /// <summary>
+    /// [延遲載入]CanvasGroup元件
+    /// </summary>
+    /*濃縮成下面那一段
+    {
+    get {
+            if (_canvasGroup == null) _canvasGroup = GetComponent<CanvasGroup>();
+            return _canvasGroup;
+        }
+    }
+    */
+    private CanvasGroup canvasGroup => _canvasGroup ??= GetComponent<CanvasGroup>();
+    
+    #endregion 基礎元件
+
+    [Tooltip("UI面板預設是否開啟")]
+    public bool openOnAwake;
+ 
+    void Start()
+    {
+        Switch(openOnAwake);
+    }
+
+    /// <summary>
+    /// UI面板切換開關
+    /// </summary>
+    /// <param name="B">true 開 / flase 關</param>
+    public void Switch(bool B)
+    {
+        /*濃縮成下面那一段
+        if (B)
+        {
+            canvasGroup.alpha = 1;
+        }
+        else
+        {
+            canvasGroup.alpha = 0;
+        }
+        */
+        //?是判斷條件。如果 B 是 true，則將 canvasGroup.alpha 設為 1（完全顯示）。如果 B 是 false，則將 canvasGroup.alpha 設為 0（完全透明）。
+        canvasGroup.alpha = B ? 1 : 0;
+        canvasGroup.blocksRaycasts = B;
+    }
+    #region ContextMenu測試功能
+    [ContextMenu("面板打開")]
+    public void PanelOn()
+    {
+        Switch(true);
+    }
+    [ContextMenu("面板關閉")]
+    public void PanelOff()
+    {
+        Switch(false);
+    }
+    #endregion ContextMenu測試功能
+}
