ab1cdef11745c26742202cf3b18ab16bd168c7cf
honeykuma <howrd1648@gmail.com>
2026-08-20 20:43:45 +0800

    14:打擊時畫面震動

--- Assets/Scripts/Skill/SkillCtrl.cs
+++ Assets/Scripts/Skill/SkillCtrl.cs
@@ -1,12 +1,35 @@
 ﻿using UnityEngine;
+using Unity.Cinemachine;
 
 public class SkillCtrl : MonoBehaviour
 {
+    #region 基礎元件
+    public CinemachineImpulseSource _impulseSource;
+    public CinemachineImpulseSource impulseSource
+    {
+        get
+        {
+            if( _impulseSource == null)
+            {
+                _impulseSource = GetComponent<CinemachineImpulseSource>();
+                if( _impulseSource == null) 
+                    _impulseSource = gameObject.AddComponent<CinemachineImpulseSource>();
+            }
+            return _impulseSource;
+        }
+    }
+    #endregion 基礎元件
+
     public enum Target { None, Enemy, Player }
     [SerializeField]
     private Target target;
     [SerializeField]
     private GameObject _hitEffectObj;
+    [SerializeField]
+    private float _hitPower = 0f;
+    private bool HitShock => _hitPower > 0f;
+    [SerializeField]
+    private float _distroyTime = 2f;
     private string Tag
     {
         get
@@ -23,7 +46,7 @@ public class SkillCtrl : MonoBehaviour
 
     void Start()
     {
-        
+        Destroy(gameObject, _distroyTime);
     }
 
     /// <summary>
@@ -35,6 +58,7 @@ public class SkillCtrl : MonoBehaviour
         if(other.tag == Tag)
         {
             _hitEffectObj.SetActive(true);
+            if (HitShock) impulseSource.GenerateImpulseWithForce(_hitPower);
         }
     }
 }
